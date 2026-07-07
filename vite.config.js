7ad137eca21d1a2103619c2cc42c3728fd3bb6eb
Tech-Dev-24 <devanathan0824@gmail.com>
2026-07-07 15:30:34 +0530

    Rename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/', // 👈 replace with your actual GitHub repo name
+  base: '/Portfolio/', // 👈 replace with your actual GitHub repo name
 })
