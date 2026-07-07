b315ae96ab913f3639dac0823bffa032913cc9b1
Tech-Dev-24 <devanathan0824@gmail.com>
2026-07-07 15:27:46 +0530

    Modified

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-website/', // 👈 replace with your actual GitHub repo name
 })
